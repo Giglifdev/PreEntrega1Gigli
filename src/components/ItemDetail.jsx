5a656f0e0161bcaed2408c11b75b29b92b472752
faustog <fausg92@gmail.com>
2023-08-08 17:36:40 -0300

    update react project

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,29 +1,22 @@
 import React from "react";
-import ItemCounter from "./ItemCounter";
 import Card from "react-bootstrap/Card";
-import { useParams } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import ItemCounter from "./ItemCounter";
 
 const ItemDetail = ({ product }) => {
-  if (!product) {
-    return <div>Loading...</div>;
-  }
   return (
     <div>
-      {filteredProducts.map((p) => {
-        return (
-          <div key={p.id}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={p.image} />
-              <Card.Body>
-                <Card.Title>{p.name}</Card.Title>
-                <Card.Text>{p.stock}</Card.Text>
-                <Button variant="dark">Add to Cart</Button>
-              </Card.Body>
-              <ItemCounter />
-            </Card>
-          </div>
-        );
-      })}
+      {product && (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={product.image} />
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.stock}</Card.Text>
+            <Button variant="dark">Add to Cart</Button>
+          </Card.Body>
+          <ItemCounter />
+        </Card>
+      )}
     </div>
   );
 };
